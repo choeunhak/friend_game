c57da308ed1df32f11ff8254fbbe5a6aa6428a3f
ceh0126 <ceh0126@midasin.com>
2023-10-20 17:04:48 +0900

    deploy 폴더 추가

--- src/lib/suikaGame/object/Size.ts
+++
@@ -1,2 +0,0 @@
-export const getRenderHeight = () => Math.min(window.innerHeight - 64 - 8, 700);
-export const getRenderWidth = () => Math.round(getRenderHeight() * 0.5714);
--- src/test/index.tsx
+++
@@ -1,4 +0,0 @@
-import SuikaGame from "../lib/suikaGame";
-import './index.module.scss'
-const Test = () => <SuikaGame />
-export default Test;
