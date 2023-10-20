dd5a92eb434fac0dfeae6e87cb98bd7d177d0e47
ceh0126 <ceh0126@midasin.com>
2023-10-20 16:41:37 +0900

    init project

---
+++ src/lib/suikaGame/gameOverModal/index.tsx
@@ -0,0 +1,25 @@
+import styles from './index.module.scss';
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+interface GameOverModalProps {
+  isVisible: boolean;
+  onClick: () => void;
+  score: number;
+}
+
+const GameOverModal = ({ isVisible, onClick, score }: GameOverModalProps) => {
+  if(!isVisible) return null;
+
+  return (
+    <div className={cx('gameOverArea')}>
+      <span className={cx('text')}>GAME OVER</span>
+      <span className={cx('score')}>SCORE: {score}</span>
+      <button className={cx('btn')} onClick={onClick}>↻ TRY AGAIN?</button>
+      <a href={'https://forms.gle/QbPDG6rzT4spywyf6'} target='_blank' className={cx('formsLink')}>의견 남기기</a>
+    </div>
+  )
+}
+
+export default GameOverModal;
---
+++ src/lib/suikaGame/object/Size.ts
@@ -0,0 +1,2 @@
+export const getRenderHeight = () => Math.min(window.innerHeight - 64 - 8, 700);
+export const getRenderWidth = () => Math.round(getRenderHeight() * 0.5714);
